8df3d276d7e4e3464bacb3e536027e82055c8de4
ACS <noreply@users.noreply.github.com>
2026-09-23 23:30:24 +0300

    نشر ACS

---
+++ assets/FinancePage-CTmCPctf.js
@@ -0,0 +1,1 @@
+import{P as e,Ut as t}from"./index-AHbCv_bb.js";import{t as n}from"./MoneyView-EYpQZK2Q.js";var r={page:`_page_15jbc_1`},i=t();function a(){return(0,i.jsxs)(`div`,{className:r.page,children:[(0,i.jsx)(e,{title:`المال`,subtitle:`شو دخل وشو طلع هالشهر، شو معك هلق، ومين إلك عنده ومين إله عليك.`}),(0,i.jsx)(n,{})]})}export{a as default};
